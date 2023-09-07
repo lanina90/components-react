674ed018f9ce6d4388ad49ccbb731d09c721ef49
Kateryna Laina <boba4ok@gmail.com>
2023-09-07 17:20:39 +0300

    feature: create navigation menu 3

---
+++ src/components/Navigation3/NavigationThree.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import {NavLink} from "react-router-dom";
+// @ts-ignore
+import styles from "./navigation.module.scss"
+import {AiOutlineHome, AiOutlineMail} from "react-icons/ai";
+import {BsPerson} from "react-icons/bs";
+import {IoSettingsOutline} from "react-icons/io5";
+import {HiOutlineVideoCamera} from "react-icons/hi2";
+
+
+const NavigationThree = () => {
+  const menuItems=[
+    {icon: <AiOutlineHome/>, color: "#94a1d4", url: '#'},
+    {icon: <BsPerson/>, color: "#2b3970",url: '#'},
+    {icon: <IoSettingsOutline/>, color: "#569686",url: '#'},
+    {icon: <AiOutlineMail/>, color: "#bcf0a5",url: '#'},
+    {icon: <HiOutlineVideoCamera/>, color: "#c28da3",url: '#'},
+
+  ]
+
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  return (
+    <div className={styles.navigation}>
+      <ul>
+        {menuItems.map(({icon, color, url}, index) => (
+          <li
+            style={{ '--clr': color } as React.CSSProperties}
+            key={index}
+            className={`${styles.list} ${activeIndex === index ? styles.active : ""}`}
+            onClick={()=> setActiveIndex(index)}
+          >
+            <NavLink to={url}>
+              <span className={styles.icon}>{icon}</span>
+            </NavLink>
+          </li>
+        ))}
+        <div className={styles.indicator}></div>
+      </ul>
+    </div>
+  );
+};
+
+export default NavigationThree;
--- src/pages/Navigations.tsx
+++ src/pages/Navigations.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import NavigationOne from "../components/NavigationOne/NavigationOne";
-import NavigationTwo from "../components/NavigationTwo/NavigationTwo";
+import NavigationOne from "../components/Navigation1/NavigationOne";
+import NavigationTwo from "../components/Navigation2/NavigationTwo";
+import NavigationThree from "../components/Navigation3/NavigationThree";
 
 const Navigations = () => {
 
 
   return (
     <section className={'flex'}>
-      <NavigationOne/>
+      <div className={'flex flex-col h-full gap-[50px]'}>
+        <NavigationOne/>
+        <NavigationThree/>
+      </div>
       <NavigationTwo/>
     </section>
   );
